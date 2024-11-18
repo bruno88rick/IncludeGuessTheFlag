0e8fab687735a0e7f91987d9ea5c3500cb993744
Bruno Ricardo de Oliveira <bruno88.rick@hotmail.com>
2024-11-18 04:46:48 -0300

    Fixing some accessibility

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -29,6 +29,20 @@ struct ContentView: View {
     
     static let allCountries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"]
     
+    let labels = [
+        "Estonia": "Flag with three horizontal stripes. Top stripe blue, middle stripe black, bottom stripe white.",
+        "France": "Flag with three vertical stripes. Left stripe blue, middle stripe white, right stripe red.",
+        "Germany": "Flag with three horizontal stripes. Top stripe black, middle stripe red, bottom stripe gold.",
+        "Ireland": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe orange.",
+        "Italy": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe red.",
+        "Nigeria": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe green.",
+        "Poland": "Flag with two horizontal stripes. Top stripe white, bottom stripe red.",
+        "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red.",
+        "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background.",
+        "Ukraine": "Flag with two horizontal stripes. Top stripe blue, bottom stripe yellow.",
+        "US": "Flag with many red and white stripes, with white stars on a blue background in the top-left corner."
+    ]
+    
     @State private var countries = allCountries.shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
     @State private var showingResult = false
@@ -74,7 +88,7 @@ struct ContentView: View {
                             Button {
                                 flagTapped(number)
                             } label: {
-                                FlagImage(image: countries[number])
+                                FlagImage(image: countries[number], labels: labels)
                             }
                             //if selectedFlag was the number who was selected, apply a 360 rotarion on y axis
                             .rotation3DEffect(
--- GuessTheFlag/FlagImage.swift
+++ GuessTheFlag/FlagImage.swift
@@ -9,15 +9,17 @@ import SwiftUI
 
 struct FlagImage: View {
     var image: String
+    var labels: [String:String]
     var rotationAmount = 0.0
 
     var body: some View {
         Image(image)
             .clipShape(.capsule)
             .shadow(radius: 5)
+            .accessibilityLabel(labels[image]!)
     }
 }
 
 #Preview {
-    FlagImage(image: "France")
+    FlagImage(image: "France", labels: ["France" : "Flag of France"])
 }
